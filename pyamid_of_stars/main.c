26940c06aa414a7f5e7bad265e2aa0c16e0a2ac3
fil303 <webfil303@gmail.com>
2025-07-05 10:10:23 +0600

    pyramid_of_starts: - left to right pyramid added - right to left pyramid added - center pyramid added

--- pyamid_of_stars/main.c
+++ pyamid_of_stars/main.c
@@ -1,11 +1,58 @@
 #include<stdio.h>
 
-int main(){
-    int pyramid = 5;
+void right_to_left_pyramid(int length){
+    int pyramid_row = length;
 
-    for(int row = 1; row <= pyramid; row++){
-        for(int column = 1; column <= pyramid; column++){
+    for(int row = 1; row <= pyramid_row; row++){
+        for(int column = 1; column <= row; column++){
             printf("*");
+            if(column == row) printf("\n");
+        }
+
+    }
+}
+
+void left_to_right_pyramid(int length){
+    int pyramid_row = length;
+
+    for(int row = 1; row <= pyramid_row; row++){
+        for(int column = 1; column <= pyramid_row; column++){
+            int empty_col_in_row = pyramid_row - row;
+
+            if(empty_col_in_row < column)
+                printf("*");
+            else
+                printf(" ");
+
+            if(column == pyramid_row) printf("\n");
+        }
+
+    }
+}
+
+void center_pyramid(int length){
+    int pyramid_col = length;
+    int pyramid_row = length - 2;
+
+    for(int row = 1; row <= pyramid_row; row++){
+        for(int column = 1; column <= pyramid_col; column++){
+            int empty_col_in_row = pyramid_row - row;
+            int printable_col    = pyramid_col - empty_col_in_row;
+
+            if(
+                empty_col_in_row < column
+                && printable_col >= column
+            )   printf("*");
+            else printf(" ");
+
+            if(column == pyramid_col) printf("\n");
         }
     }
+}
+
+int main(){
+    right_to_left_pyramid(10);
+    left_to_right_pyramid(10);
+    center_pyramid(15);
+    return 0;
 }
